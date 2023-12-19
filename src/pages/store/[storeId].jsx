8d38dfe8270c6c718bcc1d784572f8a462d1b096
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2023-12-19 18:52:24 -0300

    refactor: Remove unused res

--- src/pages/store/[storeId].jsx
+++ src/pages/store/[storeId].jsx
@@ -16,7 +16,7 @@ export default function Home({ dashboardData }) {
   ) 
 }
 
-export async function getServerSideProps({query, res}) {
+export async function getServerSideProps({query}) {
   const { storeId } = query;
   const data = await readFileSync('./stores.json');
   const storesArray = await JSON.parse(data);
