611c67e6b554889ecb32deaef9d0ac64880d0d70
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2024-08-23 19:17:43 -0300

    chore: Update bling.js to include request delay

--- src/services/bling.js
+++ src/services/bling.js
@@ -3,6 +3,7 @@ import Bling from 'bling-erp.js';
 export const createBlingInstance = (apiKey) => {
   const bling = new Bling({
     apikey: apiKey,
+    requestDelay: 1000,
   });
 
   return bling;
